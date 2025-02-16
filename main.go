95cc8fb92ea2e6a816663747135c31c808cda5d8
Siraj Yesuf <sirajyesuf762@gmail.com>
2025-02-16 17:39:56 +0300

    =basic router using servemux

--- main.go
+++ main.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("hello from snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	w.Write([]byte("display a specific snippet"))
+	fmt.Fprintf(w, "display a specific snippet with id %d", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		w.Write([]byte("Method not allowed"))
+		return
+	}
+	w.Write([]byte("create a new snippet"))
+}
 
 func main() {
-	fmt.Println("hello word")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
+
+	log.Println("starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+
 }
